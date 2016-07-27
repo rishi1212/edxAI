b8df088e14499b571eb0ab455740b623fa399566
rishi <me.rishiverma@gmail.com>
2016-07-27 13:09:50 +0530

    buyLotsOfFruit

--- tutorial/buyLotsOfFruit.py
+++ tutorial/buyLotsOfFruit.py
@@ -34,10 +34,13 @@ def buyLotsOfFruit(orderList):
     """
     totalCost = 0.0
     "*** YOUR CODE HERE ***"
+    for i in orderList:
+        #print(fruitPrices.get(i[0]))
+        totalCost+=fruitPrices.get(i[0])*i[1]
     return totalCost
 
 # Main Method
 if __name__ == '__main__':
     "This code runs when you invoke the script from the command line"
     orderList = [ ('apples', 2.0), ('pears', 3.0), ('limes', 4.0) ]
-    print 'Cost of', orderList, 'is', buyLotsOfFruit(orderList)
+    print 'Cost of', orderList, 'is',buyLotsOfFruit(orderList)
