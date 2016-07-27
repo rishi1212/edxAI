23402b9ad0a5a3a07b6141db0e64e54fa3785da0
rishi <me.rishiverma@gmail.com>
2016-07-27 13:29:07 +0530

    smart shop

--- tutorial/shopSmart.py
+++ tutorial/shopSmart.py
@@ -23,22 +23,31 @@ For orders:  [('apples', 3.0)] best shop is shop2
 
 import shop
 
+
 def shopSmart(orderList, fruitShops):
     """
         orderList: List of (fruit, numPound) tuples
         fruitShops: List of FruitShops
     """
     "*** YOUR CODE HERE ***"
-    return None
+    bestCost = 9999999999
+    bestShop = None
+    for shop in fruitShops:
+        cost = shop.getPriceOfOrder(orderList)
+        if cost < bestCost:
+            bestCost = cost
+            bestShop = shop
+    return bestShop
+
 
 if __name__ == '__main__':
-  "This code runs when you invoke the script from the command line"
-  orders = [('apples',1.0), ('oranges',3.0)]
-  dir1 = {'apples': 2.0, 'oranges':1.0}
-  shop1 =  shop.FruitShop('shop1',dir1)
-  dir2 = {'apples': 1.0, 'oranges': 5.0}
-  shop2 = shop.FruitShop('shop2',dir2)
-  shops = [shop1, shop2]
-  print "For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName()
-  orders = [('apples',3.0)]
-  print "For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName()
+    "This code runs when you invoke the script from the command line"
+    orders = [('apples', 1.0), ('oranges', 3.0)]
+    dir1 = {'apples': 2.0, 'oranges': 1.0}
+    shop1 = shop.FruitShop('shop1', dir1)
+    dir2 = {'apples': 1.0, 'oranges': 5.0}
+    shop2 = shop.FruitShop('shop2', dir2)
+    shops = [shop1, shop2]
+    print "For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName()
+    orders = [('apples',3.0)]
+    print "For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName()
